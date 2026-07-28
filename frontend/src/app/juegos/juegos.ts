abea91450de6047c31f329b0a29899f1b43d3857
Isaac <A212303A@correo.uia.mx>
2026-07-28 16:59:04 -0600

    Filtración de juegos acorde a la consola perteneciente

--- frontend/src/app/juegos/juegos.ts
+++ frontend/src/app/juegos/juegos.ts
@@ -12,12 +12,29 @@ import { Pie } from '../pie/pie';
 })
 export class Juegos implements OnInit {
   juegos: Juego[] = [];
+  juegosPlay: Juego[] = [];
+  juegosXbox: Juego[] = [];
 
   constructor(private readonly juegoService: JuegosService) {}
 
   ngOnInit(): void {
     this.juegoService.getAll().subscribe(data => {
       this.juegos = data;
+
+      // Filtrar PlayStation (incluye PS2, PS3, PS4)
+      this.juegosPlay = data.filter(j =>
+        j.consola === 'PlayStation' ||
+        j.consola === 'PS2' ||
+        j.consola === 'PS3' ||
+        j.consola === 'PS4'
+      );
+
+      // Filtrar Xbox (incluye Xbox 360, Xbox One)
+      this.juegosXbox = data.filter(j =>
+        j.consola === 'Xbox' ||
+        j.consola === 'Xbox 360' ||
+        j.consola === 'Xbox One'
+      );
     });
   }
 }
